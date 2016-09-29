d1236bb4c920bd1dc9d0ec4fb14c4bbe3922130e
windybirth <windybirth@gmail.com>
2016-09-29 15:56:18 +0900

    update basic skill master edit component

--- skill/src/main/java/jp/wicresoft/controller/SkillMasterEditController.java
+++ skill/src/main/java/jp/wicresoft/controller/SkillMasterEditController.java
@@ -1,5 +1,13 @@
 package jp.wicresoft.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import jp.wicresoft.impl.SkillMasterEditImpl;
+
+@Controller
 public class SkillMasterEditController {
 
+	@Autowired
+	SkillMasterEditImpl skillMasterEditImpl;
 }
